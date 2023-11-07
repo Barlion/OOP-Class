9c9c857bbe275aae595eb653dc40d55e1bc77cbb
Barlion <bravinbarlion01@gmail.com>
2023-11-07 23:09:58 +0300

    Add files via upload

---
+++ ATM assignment/ATM/SavingAccount.java
@@ -0,0 +1,7 @@
+package ATM;
+
+public class SavingAccount extends BankAccount {
+    public SavingAccount(int acNo, String acName, String acPhone, String acEmail, double acBalance) {
+        super(acNo, acName, acPhone, acEmail, acBalance);
+    }
+}
